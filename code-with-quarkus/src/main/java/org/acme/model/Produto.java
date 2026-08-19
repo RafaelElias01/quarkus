5008242a0579d9e828841a589aede819bcc4aa69
RafaelElias01 <rafael707375@gmail.com>
2026-08-19 14:52:35 +0000

    rafael

---
+++ code-with-quarkus/src/main/java/org/acme/model/Produto.java
@@ -0,0 +1,56 @@
+package org.acme.model;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private String categoria;
+    private double preco;
+
+
+    // Construtor
+    public Produto() {}
+
+    public Produto(Long id, String nome, String categoria, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+
+}
+
